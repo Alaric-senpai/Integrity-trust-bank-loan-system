0816a0e21e211153e52a0669c0983200e4c397ee
Alaric <ckagenou96@gmail.com>
2024-06-07 21:30:06 +0300

    admin dashboard created

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -1,13 +1,26 @@
 <?php
+require '../client.php';
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Admin dashboard</title>
+    <?php require '../favicon.php' ?>
 </head>
-<body>
-    
+<body class="text-bg-dark">
+    <div class="container-full">
+
+        <menu>
+            <a href="#">Home</a>
+        <a href="#">Loans</a>
+    <a href="#">Settings</a>
+<a href="#">Logout</a>
+</menu>
+<div class="content"></div>
+lorem20
+</div>
 </body>
 </html>
